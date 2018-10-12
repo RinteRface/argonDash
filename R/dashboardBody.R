338653f5827fece985319c1d63831ab9c9a36c6a
Divad0175 <dgranjon@ymail.com>
2018-10-12 22:26:47 +0200

    rename main files and add utils functions

---
+++ R/dashboardBody.R
@@ -0,0 +1,50 @@
+#' Create a Boostrap 4 dashboard body
+#'
+#' Build an argon dashboard body
+#'
+#' @param ... Body content, slot for bs4TabItems.
+#' 
+#' @author David Granjon, \email{dgranjon@@ymail.com}
+#'
+#' @export
+argonDashBody <- function(...) {
+  shiny::tags$div(
+    ...
+  )
+}
+
+
+
+#' A container for tab items
+#'
+#' @param ... Items to put in the container. Each item should be a
+#'   \code{\link{argonTabItem}}.
+#'   
+#' @export
+argonTabItems <- function(...) {
+  lapply(list(...), tagAssert, class = "tab-pane")
+  
+  shiny::tags$div(class = "tab-content", ...)
+}
+
+#' One tab to put inside a tab items container
+#'
+#' @param tabName The name of a tab. This must correspond to the \code{tabName}
+#'   of a \code{\link{argonSidebarMenuItem}}.
+#' @param ... Contents of the tab.
+#' 
+#' @export
+argonTabItem <- function(tabName = NULL, ...) {
+  if (is.null(tabName))
+    stop("Need tabName")
+  
+  validateTabName(tabName)
+  
+  shiny::tags$div(
+    role = "tabpanel",
+    class = "tab-pane container-fluid",
+    id = paste0("shiny-tab-", tabName),
+    shiny::br(),
+    ...
+  )
+}
